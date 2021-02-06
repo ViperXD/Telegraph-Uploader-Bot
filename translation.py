d6ae2c2fcca5c1ad0ab6176aeaa94b626400e01b
Fayas <76828314+FayasOfficial@users.noreply.github.com>
2021-02-06 14:42:37 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,20 @@
+class Translation(object):
+    START_TEXT = """
+Hello {}, I' am Media or File to `Telegra.ph` Link Uploader Bot.
+"""
+    HELP_USER = """
+<b><u>Help and Informations</u></b>
+
+➠ Just give me a media under 5MB.
+➠ Then I will download it.
+➠ I will then upload it to the `Telegra.ph` link.
+"""
+
+    ABOUT_ME = """
+<b><u>Informations About Me</u></b>
+
+I am a Clone of @FnTelegraphBot. Please Join @FnProjects and @FayasChat. Click here to create a clone like me.
+"""
+    DOWNLOAD_TEXT = "<code>Downloading to My Server ...</code>"
+    UPLOADING_TEXT = "<code>Downloading Completed. Now I'am Uploading to telegra.ph Link ...</code>"
+    SOMETHING_WRONG = "Something Wrong! Click help button for more..."
