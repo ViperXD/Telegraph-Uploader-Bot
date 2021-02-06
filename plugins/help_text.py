bfaaeb1cf196fb715865f9424b53e8e201d0e80e
Fayas <76828314+FayasOfficial@users.noreply.github.com>
2021-02-06 14:21:50 +0530

    Create help_text.py

---
+++ plugins/help_text.py
@@ -0,0 +1,32 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from vars import Config
+from translation import Translation
+
+@Client.on_message(filters.command(["start"]))
+async def start(client, message):
+    await client.send_message(
+        chat_id=message.chat.id,
+        text=Translation.START_TEXT.format(message.from_user.mention),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FNPROJECTS'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FnHelpDesk')]]),
+        reply_to_message_id=message.message_id
+    )
+
+@Client.on_message(filters.command(["help"]))
+async def help(client, message):
+    await client.send_message(    
+        chat_id=message.chat.id,
+        text=Translation.HELP_USER,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FNPROJECTS'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FnHelpDesk')]]),
+        reply_to_message_id=message.message_id
+    )
+
+@Client.on_message(filters.command(["about"]))
+async def about(client, message):
+    await client.send_message(
+        chat_id=message.chat.id,
+        text=Translation.ABOUT_TEXT,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=message.message_id
+    )
