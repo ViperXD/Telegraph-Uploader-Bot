703fc8b8bc8592186d3c2c8b8b1693602a88764e
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-02-20 13:05:43 +0530

    Update telegraph_upload.py

--- plugins/telegraph_upload.py
+++ plugins/telegraph_upload.py
@@ -6,7 +6,7 @@ from translation import Translation
 
 @Client.on_message(filters.media & filters.private)
 async def getmedia(client, message):
-    medianame = "FnKerala/FnTelegraphBot"
+    medianame = "FNPROJECTS/FnTelegraphBot"
     dwn = await client.send_message(chat_id = message.chat.id, text=Translation.DOWNLOAD_TEXT, parse_mode="html", disable_web_page_preview=True, reply_to_message_id=message.message_id)
     await client.download_media(message=message, file_name=medianame)
     await dwn.edit_text(text=Translation.UPLOADING_TEXT)
