948e46bf8ac901a47e78a50ea8908e114f08822c
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-02-21 10:08:47 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,3 +7,6 @@ class Config(object):
     # Get these values from my.telegram.org
     API_ID = int(os.environ["API_ID"])
     API_HASH = os.environ["API_HASH"]
+    
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
